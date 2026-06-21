656eeb2f230be32d56e2fbff97be1c4a0de95d15
Angelmykl <thomasmykl4@gmail.com>
2026-06-21 17:07:24 +0100

    fix CORS to allow all origins on Bonto

--- socialmint/backend/server.js
+++ socialmint/backend/server.js
@@ -127,11 +127,7 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
+    callback(null, true);
   },
   credentials: true,
 }));

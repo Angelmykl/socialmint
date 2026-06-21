1d7c8f519e0799ffbe894401c2c6aa3dffe642f6
Angelmykl <thomasmykl4@gmail.com>
2026-06-21 17:26:01 +0100

    fix CORS allow all origins

--- socialmint/backend/server.js
+++ socialmint/backend/server.js
@@ -125,13 +125,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: true,
   credentials: true,
 }));
 app.use(express.json());
